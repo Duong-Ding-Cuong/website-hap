d34ac4dc685c7edb6d05241d08bd330a07b493fc
Dương Đình Cường <anhkoj174@gmail.com>
2025-04-01 18:34:20 +0700

    frist update code day 1/4

--- Js.js
+++ Js.js
@@ -2,3 +2,17 @@ const change = document.getElementById("language");
 function  changeLanguage() {
     change.classList.toggle("show");
 }
+// hiệu ứng nav ẩn/hiện
+const navbar = document.getElementById('navbar');
+let lastScrollTop = 0;
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Cuộn xuống
+        navbar.classList.add('hidden');
+    } else {
+        // Cuộn lên
+        navbar.classList.remove('hidden');
+    }
+    lastScrollTop = scrollTop;
+});
